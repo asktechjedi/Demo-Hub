3eb69f8869aa66613ac0ef1a5d594889d6855796
Iamvijay24 <vijay@techjedi.in>
2025-08-22 13:19:48 +0530

    Create index.js

---
+++ mic/index.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>FaceBot Widget testing</title>
+  </head>
+  <body>
+    <div>
+     facebot widget android mic test
+    </div>
+    <div id="fb-widget-config" data-fb_id="facebot" data-partner_id="facebot"></div>
+    <script src="./widget.bundle.js"></script>
+  </body>
+</html>
